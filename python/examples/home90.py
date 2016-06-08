6d8d58c416b1a82d4ba2e1900e64f2b399d83289
Simon Monk <evilgeniusauthor@gmail.com>
2016-06-08 12:34:29 +0100

    fixed test program

--- python/examples/home90.py
+++ python/examples/home90.py
@@ -1,4 +1,5 @@
 from servosix import ServoSix
+import time
 
 ss = ServoSix()
 
@@ -6,5 +7,9 @@ ss.set_servo(1, 90)
 ss.set_servo(2, 90)
 ss.set_servo(3, 90)
 ss.set_servo(4, 90)
-  
+
+time.sleep(1)
+
+print("Servos in 90 degree position")
+
 ss.cleanup()
