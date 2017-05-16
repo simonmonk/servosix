88aa4826b299d805abb9b6c10541fc16b1fe9654
Simon Monk <srmonk@gmail.com>
2017-05-16 22:20:45 +0100

    Update servod.c
    Fix for Pi3

--- python/servod.c
+++ python/servod.c
@@ -957,7 +957,7 @@ get_model_and_revision(void)
 
 	if (strstr(modelstr, "BCM2708"))
 		board_model = 1;
-	else if (strstr(modelstr, "BCM2709"))
+	else if (strstr(modelstr, "BCM2709") || strstr(modelstr, "BCM2835"))
 		board_model = 2;
 	else
 		fatal("servod: Cannot parse the hardware name string\n");
